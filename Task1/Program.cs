94523d94d1f9eb78c3db8771b47e11d0bd10f1f7
GRIGORY <bondarenko_grisha@vk.com>
2023-02-06 00:00:13 +0700

    create Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число
+ и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+int SecondDigit(int number)
+{
+int DigitOne = number / 10;
+int DigitTwo = DigitOne % 10;
+return DigitTwo;
+}
+
+Console.WriteLine("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Вторая цифра этого числа {SecondDigit(number)}");
